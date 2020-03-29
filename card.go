fef861d1494cbb5ac10a669f98219ad87f2ad237
Tom Moir <tommoir@jigsaw.xyz>
2020-03-29 10:31:12 +0100

    adds error handling for card topup and deduct fns

--- card.go
+++ card.go
@@ -6,15 +6,42 @@ import (
 )
 
 var maxBalance = 90
+var minBalance = 0
+
+func negativeValueCheck(n int) error {
+	if n < 0 {
+		return errors.New("Cannot use negative value")
+	}
+	return nil
+}
 
 type card struct {
 	balance int
 }
 
 func (c *card) topup(n int) error {
+	err := negativeValueCheck(n)
+	if err != nil {
+		return err
+	}
+
 	nb := c.balance + n
 	if nb > maxBalance {
 		return errors.New("Cannot exceed maximum balance of " + strconv.Itoa(maxBalance))
+	}
+	c.balance = nb
+	return nil
+}
+
+func (c *card) deduct(n int) error {
+	err := negativeValueCheck(n)
+	if err != nil {
+		return err
+	}
+
+	nb := c.balance - n
+	if nb < minBalance {
+		return errors.New("Cannot reduce below minimum balance of " + strconv.Itoa(minBalance))
 	}
 	c.balance = nb
 	return nil
--- card_test.go
+++ card_test.go
@@ -18,11 +18,37 @@ func TestTopup(t *testing.T) {
 	assert.Equal(t, c.balance, 5, "expected balance to equal 5")
 }
 
-func TestTopupLimit(t *testing.T) {
+func TestTopupMaxLimit(t *testing.T) {
 	c := card{90}
 	err := c.topup(1)
-	assert.NotNil(t, err, "expected err not to be nil")
+	assert.EqualError(t, err, "Cannot exceed maximum balance of 90", "expected max balance error")
 	assert.Equal(t, c.balance, 90, "expected balance to be 90")
-	_, isErr := err.(error)
-	assert.True(t, isErr, "expected err to be of type error")
+}
+
+func TestTopupNegativeValue(t *testing.T) {
+	c := card{}
+	err := c.topup(-1)
+	assert.EqualError(t, err, "Cannot use negative value", "expected negative value error")
+	assert.Equal(t, c.balance, 0, "expected balance to equal 0")
+}
+
+func TestDeduct(t *testing.T) {
+	c := card{10}
+	err := c.deduct(2)
+	assert.Nil(t, err, "expected err to be nil")
+	assert.Equal(t, c.balance, 8, "expected balance to equal 8")
+}
+
+func TestDeductMinLimit(t *testing.T) {
+	c := card{0}
+	err := c.deduct(1)
+	assert.EqualErrorf(t, err, "Cannot reduce below minimum balance of 0", "expected min balance error")
+	assert.Equal(t, c.balance, 0, "expected balance to be 0")
+}
+
+func TestDeductNegativeValue(t *testing.T) {
+	c := card{5}
+	err := c.deduct(-1)
+	assert.EqualError(t, err, "Cannot use negative value", "expected negative value error")
+	assert.Equal(t, c.balance, 5, "expected balance to equal 5")
 }
